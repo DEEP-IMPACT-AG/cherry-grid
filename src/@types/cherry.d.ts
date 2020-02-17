95e19cbee516cf6e64d6c313c6c49d90bbc1c8b9
James Wong <james.wtc@gmail.com>
2020-02-17 18:09:02 +0100

    Fix

--- src/@types/cherry.d.ts
+++ src/@types/cherry.d.ts
@@ -2,58 +2,52 @@ import * as React from "react";
 
 type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
 
-export interface ContainerProps extends React.HTMLAttributes {
-	children: any;
-	className?: string;
-	fluid?: boolean;
-	child?: boolean;
+export interface ContainerProps extends React.HTMLAttributes<HTMLElement> {
+  fluid?: boolean;
+  child?: boolean;
 }
 
 export interface RowProps extends Omit<React.HTMLProps<HTMLElement>, "form"> {
-	children: any;
-	className?: string;
-	alignItems?: string;
-	justifyContent?: string;
+  alignItems?: string;
+  justifyContent?: string;
 }
 
 export interface ColProps extends Omit<React.HTMLProps<HTMLElement>, "form"> {
-	textAlign?: string;
-	className?: string;
-	id?: string;
-	children?: any;
-	xs?: number;
-	sm?: number;
-	md?: number;
-	lg?: number;
-	xl?: number;
-	xxl?: number;
-	xxxl?: number;
-	first?: boolean;
-	firstSm?: boolean;
-	firstMd?: boolean;
-	firstLg?: boolean;
-	firstXl?: boolean;
-	firstXxl?: boolean;
-	firstXxxl?: boolean;
-	last?: boolean;
-	lastSm?: boolean;
-	lastMd?: boolean;
-	lastLg?: boolean;
-	lastXl?: boolean;
-	lastXxl?: boolean;
-	lastXxxl?: boolean;
+  textAlign?: string;
+  id?: string;
+  xs?: number;
+  sm?: number;
+  md?: number;
+  lg?: number;
+  xl?: number;
+  xxl?: number;
+  xxxl?: number;
+  first?: boolean;
+  firstSm?: boolean;
+  firstMd?: boolean;
+  firstLg?: boolean;
+  firstXl?: boolean;
+  firstXxl?: boolean;
+  firstXxxl?: boolean;
+  last?: boolean;
+  lastSm?: boolean;
+  lastMd?: boolean;
+  lastLg?: boolean;
+  lastXl?: boolean;
+  lastXxl?: boolean;
+  lastXxxl?: boolean;
 }
 
 declare class Container<T = ContainerProps> extends React.Component<
-	ContainerProps
+  ContainerProps
 > {}
 
 declare class Row<T = { [key: string]: any }> extends React.Component<
-	RowProps
+  RowProps
 > {}
 
 declare class Col<T = { [key: string]: any }> extends React.Component<
-	ColProps
+  ColProps
 > {}
 
 export { Container, Col, Row };
